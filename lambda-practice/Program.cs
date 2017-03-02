cbd3b701caae4515a76f67ad9f1e96e29aa616dc
Yamil Elías <yamileliassoto@gmail.com>
2017-03-02 12:39:36 -0700

    List Employees with department in Chihuahua

--- lambda-practice/Program.cs
+++ lambda-practice/Program.cs
@@ -12,20 +12,30 @@ namespace lambda_practice
 
             using (var ctx = new DatabaseContext())
             {
+                var employees = ctx.Employees.ToList();
+
+                foreach (var employee in employees)
+                {
+                    Console.WriteLine(employee.FirstName);
+                }
+
                 //1. Listar todos los empleados cuyo departamento tenga una sede en Chihuahua
-               
+                // var emps = ctx.Employees.Where(e => e.FirstName == "Juan").ToList();
+                var chihuahua_employees = ctx.Employees.Where(e => e.City.Id.Equals(1)).ToList();
+
+                Console.WriteLine(chihuahua_employees);
 
                 //2. Listar todos los departamentos y el numero de empleados que pertenezcan a cada departamento.
-               
+
 
                 //3. Listar todos los empleados remotos. Estos son los empleados cuya ciudad no se encuentre entre las sedes de su departamento.
-             
+
 
                 //4. Listar todos los empleados cuyo aniversario de contratación sea el próximo mes.
-              
+
 
                 //Listar los 12 meses del año y el numero de empleados contratados por cada mes.
-              
+
 
             }
 
