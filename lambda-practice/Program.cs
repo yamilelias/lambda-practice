1db5bac3aff30ef7fa1a2ef540926181b52807cf
Yamil Elías <yamileliassoto@gmail.com>
2017-03-06 19:48:29 -0700

    List all the remote employees

--- lambda-practice/Program.cs
+++ lambda-practice/Program.cs
@@ -12,6 +12,8 @@ namespace lambda_practice
 
             using (var ctx = new DatabaseContext())
             {
+                Console.WriteLine("Todos los empleados:");
+
                 var employees = ctx.Employees.ToList();
 
                 foreach (var employee in employees)
@@ -19,35 +21,46 @@ namespace lambda_practice
                     Console.WriteLine(employee.FirstName);
                 }
 
-                Console.WriteLine("===============================================");
-                //1. Listar todos los empleados cuyo departamento tenga una sede en Chihuahua
+                ////1. Listar todos los empleados cuyo departamento tenga una sede en Chihuahua
+                Console.WriteLine("Listar todos los empleados cuyo departamento tenga una sede en Chihuahua.");
+
                 // var emps = ctx.Employees.Where(e => e.FirstName == "Juan").ToList();
                 var chihuahua_employees = ctx.Employees.Where(e => e.City.Id.Equals(1)).ToList();
 
-                foreach(var employee in chihuahua_employees)
+                foreach (var employee in chihuahua_employees)
                 {
                     Console.WriteLine(employee.FirstName);
                 }
 
-                Console.WriteLine("===============================================");
-                //2. Listar todos los departamentos y el numero de empleados que pertenezcan a cada departamento.
+                ////2. Listar todos los departamentos y el numero de empleados que pertenezcan a cada departamento.
+                Console.WriteLine("Listar todos los departamentos y el numero de empleados que pertenezcan a cada departamento.");
+
                 var departments = ctx.Departments.ToList();
 
                 foreach (var department in departments)
                 {
                     // Get employees from that department
                     var department_employees = ctx.Employees.Where(e => e.DepartmentId.Equals(department.Id));
-                    Console.WriteLine($"Department {department.Name} has a total of {department_employees.Count()} employees in the department.");
+                    Console.WriteLine($"El departamento {department.Name} tiene un total de {department_employees.Count()} empleados.");
                 }
 
                 //3. Listar todos los empleados remotos. Estos son los empleados cuya ciudad no se encuentre entre las sedes de su departamento.
+                Console.WriteLine("Listar todos los empleados remotos. Estos son los empleados cuya ciudad no se encuentre entre las sedes de su departamento.");
 
+                var join_table = ctx.Employees.Where(
+                    employee => employee.Department.Cities.Any(
+                        department => department.Name != employee.City.Name));
+
+                foreach (var employee in join_table)
+                {
+                    Console.WriteLine($"El empleado {employee.FirstName} es remoto.");
+                }
 
                 //4. Listar todos los empleados cuyo aniversario de contratación sea el próximo mes.
+                
 
-
-                //Listar los 12 meses del año y el numero de empleados contratados por cada mes.
-
+                //5. Listar los 12 meses del año y el numero de empleados contratados por cada mes.
+                
 
             }
 
